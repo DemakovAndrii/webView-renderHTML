408e83c8948c4383016bf7141a3e477cde5a78ff
Андрей Демаков <demakov1155@gmail.com>
2022-11-28 12:31:06 +0200

    renderHTML

--- App.tsx
+++ App.tsx
@@ -2,12 +2,26 @@ import React from 'react';
 import {StyleSheet, View} from 'react-native';
 import {WebView} from 'react-native-webview';
 
-const LINK = 'https://www.youtube.com/';
+import {useWindowDimensions} from 'react-native';
+import RenderHtml from 'react-native-render-html';
+
+const source = {
+  html: `
+<h1 style='text-align:center;'>
+  html text
+</h1>`,
+};
+
+const LINK = 'https://www.google.com/';
 
 const App = () => {
+  const {width} = useWindowDimensions();
   return (
-    <View style={styles.link}>
-      <WebView source={{uri: LINK}} onLoad={() => console.log('asdasd')} />
+    <View>
+      <RenderHtml contentWidth={width} source={source} />
+      <View style={styles.link}>
+        <WebView source={{uri: LINK}} onLoad={() => console.log('loaded')} />
+      </View>
     </View>
   );
 };
